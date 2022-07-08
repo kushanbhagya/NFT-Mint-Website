5f24837a1ad4b1824e46c6eebc9808d428394a22
Kushan Bhagya <73896856+kushanbhagya@users.noreply.github.com>
2022-07-09 00:41:56 +0530

    Frontend without CSS

--- full-mint-website/src/App.js
+++ full-mint-website/src/App.js
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import './App.css';
+import MainMint from './MainMint';
+import NavBar from './NavBar';
 
 function App() {
+  const [accounts, setAccounts] = useState([]);
+
   return (
-    <div className="App">
-      
-    </div>
-  );
+  <div className="App">
+    <NavBar accounts={accounts} setAccounts={setAccounts} />
+    <MainMint accounts={accounts} setAccounts={setAccounts} />
+  </div>);
 }
 
 export default App;
